e49a1d6c636b82236f9e95fdba650f95a25009c1
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-11-14 16:32:38 +0100

    routes and components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Cats from "./pages/cats/index";
+import Dogs from "./pages/dogs/index";
+import Home from "./pages/home/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/cats" element={<Cats />} />
+      </Routes>
     </div>
   );
 }
